9e8179969e3db79254e544df2a56f0eb003aae80
Shashank05101999 <shashankyadav05101999@gmail.com>
2024-07-11 12:33:16 +0530

    Add Favicon Icon

--- src/Components/SearchResultContainer.js
+++ src/Components/SearchResultContainer.js
@@ -24,7 +24,7 @@ const SearchResultContainer = () => {
   };
 
   return (
-    <div className="mt-10   ">
+    <div className="mt-10  w-[150rem] ">
       <div className="flex flex-col px-3  items-center">
         <div className="p-2 m-2">
           {searchedVideos?.map((video) => (
@@ -38,9 +38,9 @@ const SearchResultContainer = () => {
                   alt="thumbnail"
                   src={video?.snippet?.thumbnails?.medium?.url}
                 />
-                <ul className="flex flex-col justify-start ml-5 w-96">
+                <ul className="flex flex-col justify-start ml-5 w-[40rem]">
                   <li className=" py-2 text-2xl ">{video?.snippet?.title}</li>
-                  <li className="text-gray-500 text-[18px]">
+                  <li className="text-gray-500 text-[18px] ">
                     {video?.snippet?.channelTitle}
                   </li>
                   <li className="text-gray-500 text-[18px]">
@@ -53,7 +53,7 @@ const SearchResultContainer = () => {
                     ).toFixed(1)}{" "}
                     days ago
                   </li>
-                  <li className="text-gray-500 mt-2 text-[15px]">
+                  <li className="text-gray-500 mt-2 text-[15px] w-[40rem] ">
                     {video?.snippet?.description}
                   </li>
                 </ul>
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 const SideBar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
   return !isMenuOpen ? null : (
-    <div className="p-5 shadow-lg w-60 py-9 px-7 h-[90vh] ">
+    <div className="p-5 shadow-lg w-60 py-9 px-16 h-[100vh] ">
       <ul className="text-center py-1 cursor-pointer ">
         <li className="hover:font-bold">
-          <Link to={"/"}>home</Link>
+          <Link to={"/"}>Home</Link>
         </li>
         <li className="hover:font-bold">Videos </li>
         <li className="hover:font-bold">Live </li>
       </ul>
-      <h1 className="font-bold text-xl text-center py-2">Subscriptions</h1>
+      <h1 className="font-bold text-xl text-center  mt-3 mb-3">
+        Subscriptions
+      </h1>
 
       <ul className="text-center cursor-pointer">
         <li className="hover:font-bold">Music </li>
@@ -20,19 +22,21 @@ const SideBar = () => {
         <li className="hover:font-bold">Gaming </li>
         <li className="hover:font-bold">Movie </li>
       </ul>
-      <h1 className="font-bold text-xl text-center py-2 ">Watch Later</h1>
+      <h1 className="font-bold text-xl text-center  mt-3 mb-3">About you</h1>
       <ul className="text-center cursor-pointer">
-        <li className="hover:font-bold">Music </li>
-        <li className="hover:font-bold">Sports </li>
-        <li className="hover:font-bold">Gaming </li>
-        <li className="hover:font-bold">Movie </li>
+        <li className="hover:font-bold"> History </li>
+        <li className="hover:font-bold">Playlists </li>
+        <li className="hover:font-bold"> Music </li>
+        <li className="hover:font-bold"> Kids Area </li>
       </ul>
-      <h1 className="font-bold text-xl text-center py-2">Accounts</h1>
+      <h1 className="font-bold text-xl text-center mt-3 mb-3">Settings</h1>
       <ul className="text-center cursor-pointer">
-        <li className="hover:font-bold">Setting </li>
-        <li className="hover:font-bold">privacy policy </li>
-        <li className="hover:font-bold">private </li>
-        <li></li>
+        <li className="hover:font-bold">Account </li>
+        <li className="hover:font-bold">Privacy policy </li>
+        <li className="hover:font-bold">Help </li>
+        <li className="hover:font-bold">FeedBack</li>
+        <li className="hover:font-bold">Report history</li>
+        <li className="hover:font-bold"> FeedBack</li>
       </ul>
     </div>
   );

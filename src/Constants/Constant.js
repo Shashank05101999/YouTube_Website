55ba2f531f6f083682efabd9b99ba1b323d03679
Shashank05101999 <shashankyadav05101999@gmail.com>
2024-07-11 11:32:47 +0530

    fix issue:search API

--- src/Constants/Constant.js
+++ src/Constants/Constant.js
@@ -7,7 +7,7 @@ export const YOUTUBE_VIDEO_API =
 export const YOUTUBE_VIDEO_WATCH_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=${GOOGLE_API_KEY}&id=`;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_SEARCH_VIDEO_WITH_QUERY_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
 
